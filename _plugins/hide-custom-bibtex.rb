a65261f9c7a9577a9c096fa423c91a019ee1adc8
systemreliability <51009183+systemreliability@users.noreply.github.com>
2026-05-08 11:22:53 +0200

    Fix publication badge spacing and PlumX loading

--- _plugins/hide-custom-bibtex.rb
+++ _plugins/hide-custom-bibtex.rb
@@ -1,11 +1,12 @@
 module Jekyll
   module HideCustomBibtex
     def hideCustomBibtex(input)
-	    keywords = @context.registers[:site].config['filtered_bibtex_keywords']
+      keywords = Array(@context.registers[:site].config['filtered_bibtex_keywords'])
+      keywords |= ['openalex_id']
 
-	    keywords.each do |keyword|
-		    input = input.gsub(/^.*\b#{keyword}\b *= *\{.*$\n/, '')
-	    end
+      keywords.each do |keyword|
+        input = input.gsub(/^.*\b#{keyword}\b *= *\{.*$\n/, '')
+      end
 
       # Clean superscripts in author lists
       input = input.gsub(/^.*\bauthor\b *= *\{.*$\n/) { |line| line.gsub(/[*†‡§¶‖&^]/, '') }
